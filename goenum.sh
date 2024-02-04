309576ad6b13015cedb441b4b3ac15224232131d
Francis Skibicki III <142934183+CyberLocc@users.noreply.github.com>
2024-02-04 15:06:21 -0700

    Update goenum.sh

--- goenum.sh
+++ goenum.sh
@@ -13,44 +13,44 @@ cat << "EOF"
 
 EOF
 
-### Check for and Install Go if needed ###
-set -e
-
-GO_VERSION=$(curl -s https://golang.org/VERSION?m=text)
-GO_ARCHIVE="$GO_VERSION.linux-arm64.tar.gz"
-GO_LINK="https://golang.org/dl/$GO_ARCHIVE"
-
+# Check if Go is installed
 if ! command -v go &> /dev/null; then
-    echo "Installing GoLang..."
-    curl -s -o /tmp/$GO_ARCHIVE $GO_LINK
-    sudo rm -rf /usr/local/go
-    sudo tar -C /usr/local -xzf /tmp/$GO_ARCHIVE
-    rm /tmp/$GO_ARCHIVE
+    echo "GoLang is not installed. Run the following command to install:"
+    echo "curl -s -o /tmp/$GO_ARCHIVE $GO_LINK && sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/$GO_ARCHIVE && rm /tmp/$GO_ARCHIVE"
 fi
 
-### Install assetfinder if needed ###
+# Check if assetfinder is installed
 if ! command -v assetfinder &> /dev/null; then
-    echo "Installing assetfinder..."
-    go install github.com/tomnomnom/assetfinder@latest 
+    echo "assetfinder is not installed. Run the following command to install:"
+    echo "go install github.com/tomnomnom/assetfinder@latest"
 fi 
 
-### Install amass if needed ###
+# Check if amass is installed
 if ! command -v amass &> /dev/null; then
-    echo "Installing amass..."	
-    go install -v github.com/owasp-amass/amass/v4/...@master 
+    echo "amass is not installed. Run the following command to install:"
+    echo "go install -v github.com/owasp-amass/amass/v4/...@master"
 fi 
 
-### Install httprobe if needed ### 
+# Check if httprobe is installed
 if ! command -v httprobe &> /dev/null; then
-    echo "Installing httprobe..."	
-    go install github.com/tomnomnom/httprobe@latest 
+    echo "httprobe is not installed. Run the following command to install:"
+    echo "go install github.com/tomnomnom/httprobe@latest"
 fi 
 
-### Install gowitness if needed ### 
-if ! command -v gowitness &> /dev/null; then
-    echo "Installing gowitness..."	
-    go install github.com/sensepost/gowitness@latest
+# Check if waybackurls is installed
+if ! command -v waybackurls &> /dev/null; then
+    echo "waybackurls is not installed. Run the following command to install:"
+    echo "GO111MODULE=on go install github.com/tomnomnom/waybackurls@latest && export PATH=$PATH:$(go env GOPATH)/bin"
 fi 
+
+# Check if gowitness is installed
+if ! command -v gowitness &> /dev/null; then
+    echo "gowitness is not installed. Run the following command to install:"
+    echo "go install github.com/sensepost/gowitness@latest"
+fi 
+
+# Add the GOBIN directory to the PATH
+export PATH=$PATH:$(go env GOPATH)/bin 
 
 ### Prompt for URL ###
 read -p "Enter the target URL: " url
@@ -63,14 +63,11 @@ user_home="$HOME"
 
 ### Create Target Directories ###
 if [ ! -d "$user_home/$url" ]; then
-    mkdir -p "$user_home/$url/recon/scans" 2> error.log
     mkdir -p "$user_home/$url/recon/eyewitness" 2> error.log
     mkdir -p "$user_home/$url/recon/wayback/params" 2> error.log
     mkdir -p "$user_home/$url/recon/wayback/extensions" 2> error.log
 fi
 
-touch "$user_home/$url/recon/alive.txt"
-touch "$user_home/$url/recon/subdomains.txt"
 
 ### Harvest subdomains with assetfinder ### 
 echo "[+] Harvesting subdomains with assetfinder..." 
@@ -78,7 +75,7 @@ assetfinder "$url" >> "$user_home/$url/recon/afassets.txt"
 cat "$user_home/$url/recon/afassets.txt" | grep "$url" >> "$user_home/$url/recon/subdomains.txt"
 rm "$user_home/$url/recon/afassets.txt"
 
-### If prompted run amass commands ###
+### Prompt to run Amass commands ###
 if [ "$run_amass" = "y" ]; then
     ### Harvest more subdomains with amass ### 
     echo "[+] Harvesting subdomains with Amass..." 
@@ -89,16 +86,29 @@ fi
 
 ### Probe subdomains with httprobe ###
 echo "[+] Probing subdomains with httprobe..."
-httprobe < "$user_home/$url/recon/subdomains.txt" | sort -u | sed 's/^http\(\|s\):\/\///g' > "$user_home/$url/recon/active.txt"
+httprobe < "$user_home/$url/recon/subdomains.txt" | sed 's/^http\(\|s\):\/\///g' > "$user_home/$url/recon/active.txt"
+sort -u $user_home/$url/recon/active.txt > $user_home/$url/recon/alive.txt
+rm $user_home/$url/recon/active.txt
+
+### Subdomain takeover with Subjack ###
+echo "[+] Checking for possible subdomain takeover..."
+ 
+if [ ! -f "$url/recon/potential_takeovers/potential_takeovers.txt" ];then
+	touch $user_home/$url/recon/potential_takeovers.txt
+fi
+ 
+subjack -w $user_home/$url/recon/subdomains.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $user_home/$url/recon/potential_takeovers.txt
 
 ### Scan subdomains with Nmap ### 
 echo "[+] Scanning Domains for open ports..." 
-nmap -iL "$user_home/$url/recon/active.txt" -T4 -oA "$user_home/$url/recon/scans/scanned.txt" -Pn > /dev/null 2>&1
+nmap -iL "$user_home/$url/recon/active.txt" -T4 -oA "$user_home/$url/recon/scanned.txt" -Pn > /dev/null 2>&1
+echo "[+] Nmap scan completed"
 
 ### Scraping Wayback Data ###
 echo "[+] Scraping wayback data..." 
 cat "$user_home/$url/recon/subdomains.txt" | waybackurls >> "$user_home/$url/recon/waybackurls.txt"
 sort -u "$user_home/$url/recon/waybackurls.txt"
+echo "[+] Wayback scraping completed"
 
 ### Pulling and compiling all possible params found in wayback data ###
 echo "[+] Pulling and compiling all possible params found in wayback data..."
@@ -131,14 +141,13 @@ for line in $(cat "$user_home/$url/recon/waybackurls.txt"); do
 done
 
 ### Remove temporary files ###
-rm "$user_home/$url/recon/wayback/extensions/js1.txt"
-rm "$user_home/$url/recon/wayback/extensions/jsp1.txt"
-rm "$user_home/$url/recon/wayback/extensions/json1.txt"
-rm "$user_home/$url/recon/wayback/extensions/php1.txt"
-rm "$user_home/$url/recon/wayback/extensions/aspx1.txt"
+[ -f "$user_home/$url/recon/wayback/extensions/js1.txt" ] && rm "$user_home/$url/recon/wayback/extensions/js1.txt"
+[ -f "$user_home/$url/recon/wayback/extensions/jsp1.txt" ] && rm "$user_home/$url/recon/wayback/extensions/jsp1.txt"
+[ -f "$user_home/$url/recon/wayback/extensions/json1.txt" ] && rm "$user_home/$url/recon/wayback/extensions/json1.txt"
+[ -f "$user_home/$url/recon/wayback/extensions/php1.txt" ] && rm "$user_home/$url/recon/wayback/extensions/php1.txt"
+[ -f "$user_home/$url/recon/wayback/extensions/aspx1.txt" ] && rm "$user_home/$url/recon/wayback/extensions/aspx1.txt"
 
 ### Announce Results ### 
 echo "[+] Enumeration Complete" 
-echo "Results @ $user_home/$url/recon"
-
+echo "Results @ $user_home/$url/recon/subdomains.txt"
 
